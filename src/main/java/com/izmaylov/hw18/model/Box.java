e60c4da6a049d73ec20d66b31df3337e21806271
izmaylov <n.izmaylov22@gmail.com>
2021-12-20 15:39:22 +0200

    add hw18

---
+++ src/main/java/com/izmaylov/hw18/model/Box.java
@@ -0,0 +1,19 @@
+package com.izmaylov.hw18.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@ToString
+public class Box {
+    private String from;
+    private String material;
+    private String color;
+    private MaxLiftingCapacity maxLiftingCapacity = new MaxLiftingCapacity();
+    private Cargo cargo = new Cargo();
+    private LocalDateTime deliveryDate;
+}
---
+++ src/main/java/com/izmaylov/hw18/model/MaxLiftingCapacity.java
@@ -0,0 +1,13 @@
+package com.izmaylov.hw18.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class MaxLiftingCapacity {
+    private String unit;
+    private int value;
+}
